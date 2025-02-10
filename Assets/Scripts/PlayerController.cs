6fdb2ecebec05204860b2b8d39c0faf3dde6bab8
AfroTidus <AfroTidus@gmail.com>
2025-02-10 18:26:04 +0000

    Third person player controller + camera
    Working third person player controller and camera. Can be improved in the future but currently works.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,9 @@ public class PlayerController : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
+        float gravity = -9.81f;
+        Vector3 velocity = new Vector3(0, gravity * Time.deltaTime, 0);
+
         if (direction.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
@@ -28,7 +31,11 @@ public class PlayerController : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            characterController.Move(moveDir.normalized * speed * Time.deltaTime);
+            characterController.Move((moveDir.normalized * speed * Time.deltaTime) + velocity);
+        }
+        else
+        {
+            characterController.Move(velocity);
         }
     }
 }
